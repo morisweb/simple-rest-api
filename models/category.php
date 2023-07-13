325582d4191329d3ab0462807a90d6f1336534f9
morisweb <57908443+morisweb@users.noreply.github.com>
2023-07-13 15:10:32 +0200

    simple rest api

--- models/category.php
+++ models/category.php
@@ -1,47 +1,50 @@
-<?php 
- class Category{
-   //database connection and table name
-   private $conn;
-   private $table_name = "category";
-
-   //object properties 
-   public $id;
-   public $name;
-   public $description;
-   public $category_name;
-   public $created;
-
-   // constructor with $db as database connection
-   public function _construct ($db){
-    $this->conn = $db;
-   }
-
-   function create(){
-
-   }
-   function read (){
-      //select all query
-      $query = "SELECT
-                c.name as category_name, p.id, p.name,p.description ,p.price, p.category_id, p.created 
+<?php
+class Category{
+  
+    // database connection and table name
+    private $conn;
+    private $table_name = "categories";
+  
+    // object properties
+    public $id;
+    public $name;
+    public $description;
+    public $created;
+  
+    public function __construct($db){
+        $this->conn = $db;
+    }
+  
+    // used by select drop-down list
+    public function readAll(){
+        //select all data
+        $query = "SELECT
+                    id, name, description
                 FROM
-                " .  $this->table_name . " p
-                LEFT JOIN 
-                 categories c
-                 ON p.category_id = c.id
+                    " . $this->table_name . "
                 ORDER BY
-                p.created DESC";  
-        // prepare query statement
-        $stmt = $this->conn->prepare($query);
-        // excute query
-        $stmt->excute();
-
+                    name";
+  
+        $stmt = $this->conn->prepare( $query );
+        $stmt->execute();
+  
         return $stmt;
-   }
-   function delete(){
-
-   }
-   function update(){
-
-   }
+    }
+    // used by select drop-down list
+public function read(){
+  
+   //select all data
+   $query = "SELECT
+               id, name, description
+           FROM
+               " . $this->table_name . "
+           ORDER BY
+               name";
+ 
+   $stmt = $this->conn->prepare( $query );
+   $stmt->execute();
+ 
+   return $stmt;
+}
 }
 ?>
